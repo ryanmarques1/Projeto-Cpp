5a3091d065aa3379ba97938bde0a3a4161c7fdf9
sidemaze@ <47296650+Sidemaze@users.noreply.github.com>
2020-08-13 22:54:42 -0300

    Projeto

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+int main(){
+	setlocale(LC_ALL, "ALL");
+	while(true){
+	     cout<<"Iniciando o Projeto..., meu pid eh de  3660"<<endl;
+		 Sleep(3000);
+	}
+ return 0;
+}
